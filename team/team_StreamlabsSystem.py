5a956ab25b340ee3efaea3e19e7c13c6c2f3f0e4
Dusty Diamond <julian@dustydiamond.de>
2022-05-30 19:18:14 +0200

    update 1.0.6
    update 1.0.6

--- team/team_StreamlabsSystem.py
+++ team/team_StreamlabsSystem.py
@@ -16,7 +16,7 @@ ScriptName = "Team Command"
 Website = "https://github.com/DustyDiamond/SL-Chatbot-Team-Plugin/blob/main/README.md"
 Description = "Answers with the Current Team you're playing with"
 Creator = "DustyDiamond"
-Version = "1.0.5"
+Version = "1.0.6"
 Command = "!team"
 
 #---------------------------------------------------------
@@ -54,52 +54,53 @@ def Execute(data):
     outputMessage = ""
     username = data.User  
     team = settings["users"] 
-    #---------------------------------------------------------
-    # !setteam branch
-    #---------------------------------------------------------
-    if data.IsChatMessage() and data.GetParam(0).lower() == (left(settings["command"],1) + "set" + right(settings["command"],(len(settings["command"])-1))) and Parent.HasPermission(data.User, settings["permission"], ""):        
-        paramCount = int(data.GetParamCount())
-        users = []
+    if data.IsChatMessage():
+        #---------------------------------------------------------
+        # !setteam branch
+        #---------------------------------------------------------
+        if data.GetParam(0).lower() == (left(settings["command"],1) + "set" + right(settings["command"],(len(settings["command"])-1))) and Parent.HasPermission(data.User, settings["permission"], ""):        
+            paramCount = int(data.GetParamCount())
+            users = []
 
-        #log("paramCount: "+ str(paramCount))
+            #log("paramCount: "+ str(paramCount))
 
-        for i in range(paramCount):
-            if i == 0:
-                continue
-            else:
-                try:
-                    users.insert(i,data.GetParam(i))
-                    #log(str(i) + ": " + data.GetParam(i))
-                except:
-                    users.append(data.GetParam(i))
-                    #log(str(i) + ": " + data.GetParam(i))
+            for i in range(paramCount):
+                if i == 0:
+                    continue
+                else:
+                    try:
+                        users.insert(i,data.GetParam(i))
+                        #log(str(i) + ": " + data.GetParam(i))
+                    except:
+                        users.append(data.GetParam(i))
+                        #log(str(i) + ": " + data.GetParam(i))
 
-            team_msg()
+                team_msg()
+                #team = settings["users"]  
+
+                if (settings["useSetteam"]):
+                    outputMessage = settings["setteam"]
+                else:
+                    outputMessage = settings["bot_response"]
+
+                #log("Team Set!")       
+
+        #---------------------------------------------------------
+        # !team branch
+        #---------------------------------------------------------
+        elif data.GetParam(0).lower() == settings["command"] and Parent.HasPermission(data.User, "Everyone", ""):
+            
+            if settings["users"] == "" or team == "":
+                return
+            
+            username = data.UserName
             #team = settings["users"]  
+            outputMessage = settings["bot_response"]
 
-            if (settings["useSetteam"]):
-                outputMessage = settings["setteam"]
-            else:
-                outputMessage = settings["bot_response"]
-
-            log("Team Set!")       
-
-    #---------------------------------------------------------
-    # !team branch
-    #---------------------------------------------------------
-    elif data.IsChatMessage() and data.GetParam(0).lower() == settings["command"] and Parent.HasPermission(data.User, "Everyone", ""):
-        
-        if settings["users"] == "" or team == "":
-            return
-        
-        username = data.UserName
-        #team = settings["users"]  
-        outputMessage = settings["bot_response"]
-
-    outputMessage = outputMessage.replace("$team", team) 
-    # final send of message
-    send_message(outputMessage.format(username))
-    return
+        outputMessage = outputMessage.replace("$team", team) 
+        # final send of message
+        send_message(outputMessage.format(username))
+        return
 
 # Misc Functions
 def log(message):
